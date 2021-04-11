35a457a1cfec30d81eedc49bfbce2b83312c55f4
Bryan Lopez <blopez24@ucsc.edu>
2021-04-11 10:49:30 -0700

    Finished RoboFriends

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,8 +2,9 @@ import React, { Component } from "react";
 import SearchBox from "../components/SearchBox";
 import CardList from "../components/CardList";
 import Scroll from "../components/Scroll";
-// import { robots } from "./robots";
+import ErrorBoundry from "../components/ErrorBoundry";
 import "./App.css";
+// import { robots } from "./robots";
 
 class App extends Component {
   constructor() {
@@ -37,7 +38,9 @@ class App extends Component {
         <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <Scroll>
-          <CardList robots={filteredRobots} />
+          <ErrorBoundry>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundry>
         </Scroll>
       </div>
     );
